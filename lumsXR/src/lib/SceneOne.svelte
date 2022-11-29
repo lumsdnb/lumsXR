47a8b5e3dc840d4e04370c582fe87bffb49973ff
lums <jeremy@lums.io>
2022-11-29 19:27:24 +0100

    init
    latest sveltekit demo project with threlte, typescript and a test scene

---
+++ lumsXR/src/lib/SceneOne.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { PerspectiveCamera,
+        DirectionalLight,
+        AmbientLight,Mesh
+     } from "@threlte/core";
+    import { MeshStandardMaterial, IcosahedronGeometry   } from "three";
+</script>
+
+
+<PerspectiveCamera
+position={{y: 5, z: 10}}
+lookAt={{y:0}}>
+</PerspectiveCamera>
+
+<DirectionalLight/>
+<AmbientLight/>
+
+<Mesh
+geometry={new IcosahedronGeometry()}
+material={new MeshStandardMaterial({color: 'seagreen'})}
+/>
---
+++ lumsXR/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import SceneOne from "$lib/SceneOne.svelte";
+import { Canvas } from "@threlte/core";
+</script>
+
+<section>
+	<h2>Scene here</h2>
+
+		<div class="canvas-wrapper">
+		<Canvas>
+			<SceneOne/>
+		</Canvas>
+	</div>
+
+</section>
+
+<style>
+	.canvas-wrapper {
+		position: relative;
+		border: 1px solid black;
+		height: 500px;
+	}
+</style>
