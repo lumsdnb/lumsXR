f22be7a862c9a0c871e99d488947262358c4449f
lums <jeremy@lums.io>
2022-12-05 23:59:20 +0100

    edit descriptions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>prototype for 3d version of <a href="https://lums.io">lums.io</a></p>
 	</footer>
 </div>
 
