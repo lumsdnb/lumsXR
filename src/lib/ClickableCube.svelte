ca29604c5c87e704f80ee0d227f39afd16832b45
lums <jeremy@lums.io>
2022-12-06 13:15:21 +0100

    Add position attributes & tex url from api

--- src/lib/ClickableCube.svelte
+++ src/lib/ClickableCube.svelte
@@ -1,15 +1,19 @@
 <script>
-  import {
 
+  export let posY=1, posZ=1
+  export let imgURL = 'https://via.placeholder.com/600/24f355'
+
+  import {
     Mesh,
     useFrame,
-    Object3DInstance,
+    Object3DInstance, useTexture
   } from "@threlte/core";
   import {
     MeshStandardMaterial,
     IcosahedronGeometry,
     GridHelper,
     AxesHelper,
+    BoxGeometry,
   } from "three";
   import { tweened } from "svelte/motion";
   import { Text } from "@threlte/extras";
@@ -22,14 +26,22 @@
     let direction = -0.01;
     r += direction;
   });
+
+  const texture = useTexture(imgURL, {
+	onError: (error) => {
+		console.warn(`An error occured loading the texture: ${error.message}`)
+	}
+})
+
 </script>
 
 <Mesh
-  geometry={new IcosahedronGeometry()}
-  material={new MeshStandardMaterial({ color: "seagreen" })}
+  geometry={new BoxGeometry()}
+  material={new MeshStandardMaterial({ texture})}
   rotation={{ y: r }}
-  position={{ x: $t }}
+  position={{ x: $t, y: posY, z: posZ }}
   interactive
+  
   on:click={() => ($t = $t > 0 ? -3 : 3)}
 >
   <Object3DInstance object={new AxesHelper(5)} />
